5d41e7b4fc80a332053ac707b11e81a76e1706e4
Daniel Chae <daniel.jiwoong@gmail.com>
2025-03-21 15:12:54 +0900

    backend set up with mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,110 +1,120 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const Person = require("./models/person.js");
 
 // App init
 const app = express();
 app.use(express.json());
 app.use(express.static("dist"));
 
+// init logging
 morgan.token("json", (request) => "\n" + JSON.stringify(request.body));
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :json"),
 );
 
-// "Database"
-let persons = [
-  {
-    id: "48482",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "50517",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "20567",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "97845",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+// // "Database"
+// let persons = [
+//   {
+//     id: "48482",
+//     name: "Arto Hellas",
+//     number: "040-123456",
+//   },
+//   {
+//     id: "50517",
+//     name: "Ada Lovelace",
+//     number: "39-44-5323523",
+//   },
+//   {
+//     id: "20567",
+//     name: "Dan Abramov",
+//     number: "12-43-234345",
+//   },
+//   {
+//     id: "97845",
+//     name: "Mary Poppendieck",
+//     number: "39-23-6423122",
+//   },
+// ];
 
 // HELPERS
+const nameExists = async (name) => {
+  try {
+    const nameRegex = new RegExp(`^${name}$`, "i");
+    const existing = await Person.findOne({ name: nameRegex });
+    return !!existing;
+  } catch (e) {
+    console.log("HERE2");
+    console.log("Error checking for existing name", error);
+    throw error;
+  }
+};
 
-const generateId = ((existing) => {
-  const pool = new Set(existing.map((x) => x.id));
-  return () => {
-    let id;
-    do {
-      id = String(Math.floor(Math.random() * 10 ** 5));
-    } while (id && pool.has(id));
-    return id;
-  };
-})(persons);
-
-const personError = (name, number) => {
+const personError = async (name, number) => {
   if (!name) return "Name cannot be empty";
-  if (
-    persons.some((p) => p.name.toLocaleLowerCase() === name.toLocaleLowerCase())
-  )
-    return "That person already exists";
+  if (await nameExists(name)) return "That person already exists";
   if (typeof number !== "string" || !number.match(/^[0-9\-\(\)]+$/)) {
     return "Phone number must be a non-empty string. Allowed chars are digits, hyphens and parentheses.";
   }
   return null;
 };
 
-const getPerson = (id) => persons.find((person) => person.id === id);
-
 // ROUTES
 
 app.get("/info", (request, response) => {
-  const body = `<p>Phonebook has info for ${persons.length} people</p>\n${new Date()}`;
+  const count = Person.countDocuments();
+  const body = `<p>Phonebook has info for ${count} people</p>\n${new Date()}`;
   response.send(body);
 });
 
-app.get("/api/persons", (request, response) => {
+app.get("/api/persons", async (request, response) => {
+  const persons = await Person.find({});
   response.json(persons);
 });
 
 app.get("/api/persons/:id", (request, response) => {
   const id = request.params.id;
-  const person = getPerson(id);
+  const person = Person.findById(id);
   if (!person) return response.status(404).end();
 
   response.json(person);
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", async (request, response) => {
   const name = request.body.name.trim();
   const number = request.body.number.trim();
 
-  const error = personError(name, number);
+  const error = await personError(name, number);
   if (error) return response.status(400).json({ error });
 
-  const person = {
-    id: generateId(),
-    name,
-    number,
-  };
-  persons = persons.concat(person);
-  response.json(person);
+  const newPerson = await Person.insertOne({ name, number });
+  response.json(newPerson);
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.put("/api/persons/:id", async (request, response) => {
   const id = request.params.id;
-  persons = persons.filter((p) => p.id !== id);
-  response.status(204).end();
+  const name = request.body.name.trim();
+  const number = request.body.number.trim();
+
+  const opts = { new: true, runValidators: true };
+  const updated = await Person.findByIdAndUpdate(id, { name, number }, opts);
+  response.json(updated);
 });
 
-const PORT = process.env.PORT || 3001;
+app.delete("/api/persons/:id", async (request, response) => {
+  const id = request.params.id;
+  try {
+    const deleted = await Person.findByIdAndDelete(id);
+    let code = deleted ? 204 : 404;
+    response.status(code).end();
+  } catch {
+    response.status(500).end();
+  }
+});
 
+// start app
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Phonebook API server running on port ${PORT}`);
 });
